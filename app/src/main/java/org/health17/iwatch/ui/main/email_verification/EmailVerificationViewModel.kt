000d626c26cbdfb4516f967a12d8dcc915375e9e
Brendan Ejike <ejike.br@gmail.com>
2020-10-25 00:07:17 +0100

    Cleanup dev
    Signed-off-by: Brendan Ejike <ejike.br@gmail.com>

--- app/src/main/java/org/health17/iwatch/ui/main/email_verification/EmailVerificationViewModel.kt
+++
@@ -1,7 +0,0 @@
-package org.health17.iwatch.ui.main.email_verification
-
-import androidx.lifecycle.ViewModel
-
-class EmailVerificationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
