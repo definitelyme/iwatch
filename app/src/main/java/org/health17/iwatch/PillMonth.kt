f53078c9d7791d6ab0e6228630a56a0f4f2294f9
Brendan E <ejike.br@gmail.com>
2020-11-07 01:36:24 +0100

    Pull Testing into dev branch (#15)
    Co-authored-by: FrankKamala <frankkamala@gmail.com>
    Co-authored-by: maraqua <martyturing@gmail.com>

---
+++ app/src/main/java/org/health17/iwatch/PillMonth.kt
@@ -0,0 +1,13 @@
+package org.health17.iwatch
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PillMonth : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pill_month)
+
+    }
+}
---
+++ app/src/main/java/org/health17/iwatch/Settings.kt
@@ -0,0 +1,11 @@
+package org.health17.iwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
